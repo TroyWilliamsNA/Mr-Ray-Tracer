ca28431bdad259ca724b87989066c319eacbc1fe
Troy Williams <troywilliamsna@gmail.com>
2019-05-04 15:46:38 -0400

    imcreased gamme, fixed shadow acne, rendered at higher AA-quality

--- MrRayTracer/MrRayTracer/MrRayTracer.cpp
+++ MrRayTracer/MrRayTracer/MrRayTracer.cpp
@@ -12,10 +12,26 @@
 #include "camera.h"
 #include <random>
 
+float drand48() {
+	return (rand() / (RAND_MAX + 1.0));
+}
+
+simplevector random_in_sphere() {
+	std::cout << "new random" << std::endl;
+	simplevector random_point(2,2,2);
+	while (random_point.squared_length() >= 1.0) {
+		random_point = 2.0*simplevector(drand48(), drand48(), drand48()) - simplevector(1,1,1);
+	}
+	return random_point;
+}
+
+
 simplevector color(const Ray& r, MeshList* world) {
 	hit_record rec;
-	if (world->hit(r, 0.0, std::numeric_limits<float>::max(), rec)) {
-		return 0.5*simplevector(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+	if (world->hit(r, 0.001, std::numeric_limits<float>::max(), rec)) {
+		simplevector target = rec.p + rec.normal + random_in_sphere();
+		std::cout << "bounce " << rec.p << std::endl;
+		return 0.5*color(Ray(rec.p, target-rec.p), world);
 	}
 	else {
 		simplevector unit_direction = unit_vector(r.direction());
@@ -28,7 +44,7 @@ void create_ppm() {
 	std::ofstream oppm("image.ppm");
 	int tx = 200;
 	int ty = 100;
-	int samples = 100;
+	int samples = 50;
 	oppm << "P3\n" << tx << " " << ty << "\n255" << std::endl;
 
 	Mesh* list[3];
@@ -44,14 +60,15 @@ void create_ppm() {
 		for (int j = 0; j < tx; j++) {
 			simplevector AA_color(0, 0, 0);
 			for (int s = 0; s < samples; s++) {
-				float u = float(j + (rand() / (RAND_MAX + 1.0))) / float(tx);
-				float v = float(i + (rand() / (RAND_MAX + 1.0))) / float(ty);
+				float u = float(j + drand48()) / float(tx);
+				float v = float(i + drand48()) / float(ty);
 				Ray curr_ray = cam.get_Ray(u, v);
 				simplevector p = curr_ray.p_at_t(2.0);
 				simplevector ray_color = color(curr_ray, world);
 				AA_color += ray_color;
 			}
 			AA_color /= float(samples);
+			AA_color = simplevector(sqrt(AA_color[0]), sqrt(AA_color[1]), sqrt(AA_color[2]));
 			int ir = int(255.99 * AA_color.r());
 			int ig = int(255.99 * AA_color.g());
 			int ib = int(255.99 * AA_color.b());
