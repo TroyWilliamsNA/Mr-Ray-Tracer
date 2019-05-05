80fe0e7af1a65180192db9d29f6913e34b9ce269
Troy Williams <troywilliamsna@gmail.com>
2019-05-05 13:33:43 -0400

    Implementing Dielectrics (broken atm)

--- MrRayTracer/MrRayTracer/material.h
+++ MrRayTracer/MrRayTracer/material.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Mesh.h"
 
+
 class material
 {
 public:
@@ -57,6 +58,8 @@ simplevector reflect(const simplevector& vec, const simplevector& normal) {
 }
 
 
+// ____________________________ METAL MATERIAL _________________________________
+
 class metal : public material {
 public:
 	metal(const simplevector& alb, float fuzz) : fuzz(fuzz), albedo(alb) {};
@@ -77,5 +80,82 @@ public:
 		return false;
 	}
 	float fuzz;
+	simplevector albedo;
+};
+
+
+// ___________ dielectric Helper Functions ___________________
+
+bool refract(const simplevector& dir, const simplevector& norm,
+	float n_ratio, simplevector& refracted) {
+	simplevector unit = unit_vector(dir);
+	float dt = dot(unit, norm);
+	float discriminant = 1.0 - n_ratio * n_ratio * (1 - dt * dt);
+	if (discriminant > 0) {
+		refracted = n_ratio * (unit - norm * dt) - norm * sqrt(discriminant);
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+float schlick(float cosine, float ref_idx) {
+	float r0 = (1 - ref_idx) / (1 + ref_idx);
+	r0 = r0 * r0;
+	return r0 + (1 - r0) * pow((1 - cosine), 5);
+}
+
+
+// ________________________ DIALETRIC MATERIAL _____________________________
+
+class dielectric : public material {
+public:
+	dielectric(simplevector alb, float refractive_index) : ref_idx(refractive_index), albedo(alb) {};
+	~dielectric() {};
+
+	virtual bool scatter(const Ray& r_in, const hit_record& rec,
+		simplevector& attenuation, Ray& scattered) const override {
+
+
+		
+		simplevector outward_normal;
+		simplevector reflected = reflect(r_in.direction(), rec.normal);
+		float n_ratio;
+		attenuation = albedo;
+		simplevector refracted;
+		float reflect_probability;
+		float cosine;
+		if (dot(r_in.direction(), rec.normal) > 0) {
+			outward_normal = -rec.normal;
+			n_ratio = ref_idx;
+			//cosine = ref_idx * dot(r_in.direction(), rec.normal) / r_in.direction().length();
+			cosine = dot(r_in.direction(), rec.normal) / r_in.direction().length();
+			cosine = sqrt(1 - ref_idx * ref_idx * (1 - cosine * cosine));
+		}
+		else {
+			outward_normal = rec.normal;
+			n_ratio = (1.0 / ref_idx);
+			cosine = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
+		}
+		if (refract(r_in.direction(), outward_normal, n_ratio, refracted)) {
+			reflect_probability = schlick(cosine, ref_idx);
+		}
+		else {
+			reflect_probability = 1.0;
+			return false;
+		}
+		if (drand48() < reflect_probability) {
+			simplevector reflected = reflect(r_in.direction(), rec.normal);
+			scattered = Ray(rec.p, reflected);
+		} 
+		else {
+			scattered = Ray(rec.p, refracted);
+		}
+		return true;
+	}
+	
+	
+	float ref_idx;
 	simplevector albedo;
 };
--- MrRayTracer/MrRayTracer/sphere.h
+++ MrRayTracer/MrRayTracer/sphere.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Mesh.h"
 
+
 class sphere : public Mesh {
 public:
 	sphere();
@@ -15,23 +16,24 @@ public:
 
 bool sphere::hit(const Ray& r, float min_t, float max_t, hit_record& rec) const {
 	float A = dot(r.direction(), r.direction());
-	float B = 2 * dot(r.direction(), r.origin() - center);
+	float B = 2 * dot(r.origin() - center, r.direction());
 	float C = (dot(r.origin() - center, r.origin() - center) - (radius * radius));
 	float desc = (B * B - (4 * A * C));
-	rec.mat_ptr = mat;
 	if (desc > 0) {
-		float t = (-B - sqrt(desc)) / (2.0 * A);
+		float t = (-B - sqrt(desc)) / (2*A);
 		if ((t < max_t) && (t > min_t)) {
 			rec.t = t;
 			rec.p = r.p_at_t(rec.t);
 			rec.normal = (rec.p - center) / radius;
+			rec.mat_ptr = mat;
 			return true;
 		}
-		t = (-B + sqrt(desc)) / (2.0 * A);
+		t = (-B + sqrt(desc)) / (2*A);
 		if ((t < max_t) && (t > min_t)) {
 			rec.t = t;
 			rec.p = r.p_at_t(rec.t);
 			rec.normal = (rec.p - center) / radius;
+			rec.mat_ptr = mat;
 			return true;
 		}
 	}
